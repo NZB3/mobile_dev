56ee7fcc6a47aad19c5d3487568e7a913c66fe4b
Николай Бухалов <ni.bukhalov@gmail.com>
2024-06-27 00:30:41 +0300

    asd;glfkjasdfg

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./views/Home/Home";
+import Profile from "./views/Profile/Profile";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Домашняя страница" }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: "Экран профиля" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
